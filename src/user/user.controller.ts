d9913efc0a8ad12393561dd7d6c211478aaf938a
柴志远 <wb.chaizhiyuan01@mesg.corp.netease.com>
2025-05-15 17:23:43 +0800

    init

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -25,7 +25,7 @@ export class UserController {
   //分页获取用户
   @Get('page')
   findAllPage(@Query() query: PageOptionsDto) {
-    return '1111'
+    return '22222'
     return this.userService.findAllPage(query);
   }
 
